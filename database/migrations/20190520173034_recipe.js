32f51dcd82cb48c986594b5d63bc1dd24f55621e
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-20 19:04:42 -0400

    migrates

---
+++ database/migrations/20190520173034_recipe.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("recipes", tbl => {
+    tbl.increments();
+
+    tbl.string("title", 255).notNullable();
+    tbl.string("mealType", 255).notNullable();
+    tbl.string("instructions", 30000000);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  // undo the operation in up
+  return knex.schema.dropTableIfExists("recipes");
+};
---
+++ database/seeds/recipes.js
@@ -0,0 +1,14 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries and resets ids
+  return knex("recipes")
+    .truncate()
+    .then(function() {
+      return knex("recipes").insert([
+        {
+          title: "",
+          mealType: "",
+          instructions: ""
+        }
+      ]);
+    });
+};

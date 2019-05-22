3232442abf12b8431458d7a67ee603820e215c38
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-22 13:15:16 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,20 @@ module.exports = {
       tableName: "ingredients"
     },
     seeds: { directory: "./database/seeds" }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
   }
 };

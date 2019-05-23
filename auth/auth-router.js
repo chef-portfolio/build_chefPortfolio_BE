723c0f4586d07663a6a2001b55633ccbddf69fab
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-23 12:09:12 -0400

    added messages

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -15,7 +15,7 @@ router.post("/register", (req, res) => {
       res.status(201).json(saved);
     })
     .catch(err => {
-      res.status(500).json(err);
+      res.status(500).json({ message: "Auth line 18" });
     });
 });
 
@@ -40,7 +40,7 @@ router.post("/login", (req, res) => {
       }
     })
     .catch(err => {
-      res.status(500).json(err);
+      res.status(500).json({ message: "Auth line 43" });
     });
 });
 
